4289bcb62a9a13f1ad837429a831971420864c8e
xzjxy <xuzhijing1995@163.com>
2023-02-08 20:11:21 +0800

    2023.2.8

--- JavaLearning/src/Chapter3/BubbleSort.java
+++ JavaLearning/src/Chapter3/BubbleSort.java
@@ -4,15 +4,22 @@ public class BubbleSort {
     //冒泡排序 0-n-1 / 0-n-2..../0-1
     public static void main(String[] args) {
         int[] arr = {0, 23, 45, 76, 12, 5, 90, 0};
-        int temp;
-        for (int i = arr.length; i >= 1; i--) {
-//            System.out.println(arr[i]);
-            for (int j = 0; j <= (i - 2); j++)
-                if (arr[j] > arr[j + 1]) {
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+//        arr = null;
+//        int arr[] = {99};
+        if (arr == null) {
+            System.out.println(arr);
+            return;
+        } else if (arr.length == 1) {
+            System.out.println(arr[0]);
+        } else {
+            int temp;
+            for (int i = arr.length; i >= 1; i--)
+                for (int j = 0; j <= (i - 2); j++)
+                    if (arr[j] > arr[j + 1]) {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
         }
         for (int j : arr) {
             System.out.print(j + " ");
