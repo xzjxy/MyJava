cd4a80d19019eacbf0eb3d4ca839313d0e95f9aa
xzjxy <xuzhijing1995@163.com>
2023-02-09 19:10:56 +0800

    2023.2.9

---
+++ JavaLearning/src/chapter4/MethodDemon.java
@@ -0,0 +1,13 @@
+package chapter4;
+
+public class MethodDemon {
+    // 定义方法/函数
+    public static void main(String[] args) {
+        System.out.println(sum(23, 24));
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
+
